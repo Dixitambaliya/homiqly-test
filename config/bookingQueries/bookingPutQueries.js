88af618c122c9a2f31a997864397db551209d357
Chintan Rabadiya <115687041+Chintan5942@users.noreply.github.com>
2025-06-29 20:21:03 +0530

    Fix Booking Approval API

--- config/bookingQueries/bookingPutQueries.js
+++ config/bookingQueries/bookingPutQueries.js
@@ -3,8 +3,8 @@ const bookingPutQueries = {
     approveOrRejectBooking: `
     UPDATE service_booking
     SET bookingStatus = ?
-    WHERE service_booking_id = ? AND vendor_id = ?
+    WHERE booking_id = ? AND vendor_id = ?
 `,
 }
 
-module.exports = bookingPutQueries; 
+module.exports = bookingPutQueries;
