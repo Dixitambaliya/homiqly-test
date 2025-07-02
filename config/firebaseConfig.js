eb4990323c2b7fa7825423f8720a4a2f39c90a52
Ambaliya Dixit <154045627+Dixitambaliya@users.noreply.github.com>
2025-07-02 15:18:21 +0530

    Fix private key formatting in Firebase configuration

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,6 +1,7 @@
 var admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
