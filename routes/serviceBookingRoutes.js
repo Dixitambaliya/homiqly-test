f78c8d485ac8d51d67c659cb47a5bcfef97db03a
dixit <dixitambaliya303@gmail.com>
2025-08-13 11:01:39 +0530

    .

--- routes/serviceBookingRoutes.js
+++ routes/serviceBookingRoutes.js
@@ -15,20 +15,12 @@ const { upload, handleUploads } = require("../middleware/upload");
 const multiUpload = upload.any();
 
 router.post('/bookservice', multiUpload, handleUploads, authenticationToken, bookService);
-
 router.get('/vendorassignedservices', authenticationToken, getVendorBookings);
-
-
 router.get('/getvendorsbytime', authenticationToken, getAvailableVendors);
-
-
-
 router.get('/userbookedservices', authenticationToken, getUserBookings);
 router.get('/get-eligible-vendors/:booking_id', authenticationToken, getEligiblevendors);
-
 router.put('/approveorrejectbooking', authenticationToken, approveOrRejectBooking);
-router.post("/assignbooking", authenticationToken, assignBookingToVendor)
-
+router.post("/assignbooking", authenticationToken, assignBookingToVendor);
 router.post("/approveandassign", authenticationToken, approveOrAssignBooking)
 
 module.exports = router;
