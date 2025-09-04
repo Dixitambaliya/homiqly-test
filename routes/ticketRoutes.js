12da96fed69df616291c71213e4af461ac1ac005
Taman Patel <taman.hansalia@gmail.com>
2025-09-04 18:13:04 +0530

    getVendorSupportTickets added

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -5,12 +5,13 @@ const {
   sendMessageToAdmins,
   getAllSupportTickets,
   deleteTicket,
+  getVendorSupportTickets,
 } = require("../controller/ticket");
 const { authenticationToken } = require("../middleware/authMiddleware");
 
 router.post("/contact", authenticationToken, sendMessageToAdmins);
 router.get("/gettickets", authenticationToken, getAllSupportTickets);
-router.get("/vendor", getVendorSupportTickets); // <-- new route
+router.get("/vendor", authenticationToken, getVendorSupportTickets); // <-- new route
 router.delete("/deleteticket/:ticket_id", authenticationToken, deleteTicket);
 
 module.exports = router;
