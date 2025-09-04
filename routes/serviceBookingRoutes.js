45cf15bde575fec2634449658b53f4c6a3dccb84
dixit <dixitambaliya303@gmail.com>
2025-09-04 12:13:17 +0530

    platform fee fixed

--- routes/serviceBookingRoutes.js
+++ routes/serviceBookingRoutes.js
@@ -15,9 +15,15 @@ const { upload, handleUploads } = require("../middleware/upload");
 const multiUpload = upload.any();
 
 router.post('/bookservice', multiUpload, handleUploads, authenticationToken, bookService);
+
 router.get('/vendorassignedservices', authenticationToken, getVendorBookings);
+
 router.get('/getvendorsbytime', authenticationToken, getAvailableVendors);
+
+
 router.get('/userbookedservices', authenticationToken, getUserBookings);
+
+
 router.get('/get-eligible-vendors/:booking_id', authenticationToken, getEligiblevendors);
 router.put('/approveorrejectbooking', authenticationToken, approveOrRejectBooking);
 router.post("/assignbooking", authenticationToken, assignBookingToVendor);
