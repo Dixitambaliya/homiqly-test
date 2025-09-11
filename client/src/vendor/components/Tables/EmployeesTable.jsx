f913769d80c4b9b3c54e76ca2d5df5a754bffd8f
Taman Patel <taman.hansalia@gmail.com>
2025-09-11 18:26:33 +0530

    >edit employee for vendor fixed

--- client/src/vendor/components/Tables/EmployeesTable.jsx
+++ client/src/vendor/components/Tables/EmployeesTable.jsx
@@ -4,8 +4,9 @@ import StatusBadge from "../../../shared/components/StatusBadge";
 import { FiTrash2 } from "react-icons/fi";
 import api from "../../../lib/axiosConfig";
 import { IconButton } from "../../../shared/components/Button";
+import { Edit, Icon, Pen, Pencil } from "lucide-react";
 
-const EmployeesTable = ({ employees, isLoading, onDelete }) => {
+const EmployeesTable = ({ employees, isLoading, onDelete, onEdit }) => {
   const handleDelete = async (employee_id) => {
     if (!window.confirm("Are you sure you want to delete this employee?"))
       return;
@@ -32,7 +33,7 @@ const EmployeesTable = ({ employees, isLoading, onDelete }) => {
       title: "Name",
       render: (row) => (
         <div className="text-sm font-medium text-gray-900">
-          {row.employee_name}
+          {row.first_name} {row.last_name}
         </div>
       ),
     },
@@ -64,15 +65,26 @@ const EmployeesTable = ({ employees, isLoading, onDelete }) => {
       title: "Actions",
       key: "actions",
       render: (row) => (
-        <IconButton
-          className="text-red-600 hover:text-red-800 p-1"
-          onClick={() => handleDelete(row.employee_id)}
-          title="Delete employee"
-          icon={<FiTrash2 className="h-5 w-5" />}
-          variant="lightDanger"
-        >
-          {/* <FiTrash2 /> */}
-        </IconButton>
+        <div className="flex items-center gap-2">
+          <IconButton
+            className="text-red-600 hover:text-red-800 p-1"
+            onClick={() => handleDelete(row.employee_id)}
+            title="Delete employee"
+            icon={<FiTrash2 className="h-4 w-4" />}
+            variant="lightDanger"
+          >
+            {/* <FiTrash2 /> */}
+          </IconButton>
+          <IconButton
+            className="text-blue-600 hover:text-blue-800 p-1"
+            onClick={() => onEdit(row)}
+            title="Edit employee"
+            icon={<Pencil name="edit" className="h-4 w-4" />}
+            variant="lightGhost"
+          >
+            {/* <FiEdit /> */}
+          </IconButton>
+        </div>
       ),
     },
   ];
