a9fc950bbbe8a7dfaa34707bacdd21d2dfa8bf8c
dixit <dixitambaliya303@gmail.com>
2025-10-03 17:48:22 +0530

    .

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { sendOtp, verifyOtp } = require('../controller/otpController');
+const { authenticationToken } = require('../middleware/authMiddleware');
 
 // Route to send OTP
 router.post('/send-otp', sendOtp);
 
 // Route to verify OTP
-router.post('/verify-otp', verifyOtp);
+router.post('/verify-otp', authenticationToken, verifyOtp);
 
 module.exports = router;
