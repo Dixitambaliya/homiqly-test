aa7dc87916382905e17371410accb99b73668454
dixit <dixitambaliya303@gmail.com>
2025-10-09 15:59:20 +0530

    .

--- config/bookingQueries/bookingGetQueries.js
+++ config/bookingQueries/bookingGetQueries.js
@@ -24,6 +24,7 @@ const bookingGetQueries = {
     u.phone AS userPhone,
     u.address AS userAddress,
     u.flatNumber AS userFlatNumber,
+    u.state AS userState,
     u.parkingInstruction AS userParkingInstructions,
     u.postalcode AS userPostalcode,
     e.employee_id AS assignedEmployeeId,
