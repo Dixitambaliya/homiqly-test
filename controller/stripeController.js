d765f53b0b16265158be85039182ae34d420addf
dixit <dixitambaliya303@gmail.com>
2025-12-08 11:51:05 +0530

    .

--- controller/stripeController.js
+++ controller/stripeController.js
@@ -578,8 +578,8 @@ exports.stripeWebhook = asyncHandler(async (req, res) => {
             console.log("✅ Transaction committed — booking & payment completed");
 
             // // // Send emails (non-blocking)
-            sendBookingEmail(cart.user_id, { booking_id, receiptUrl });
-            sendVendorBookingEmail(cart.vendor_id, { booking_id, receiptUrl });
+            // sendBookingEmail(cart.user_id, { booking_id, receiptUrl });
+            // sendVendorBookingEmail(cart.vendor_id, { booking_id, receiptUrl });
 
             // Generate PDF after 3 seconds (non-blocking)
             setTimeout(async () => {
