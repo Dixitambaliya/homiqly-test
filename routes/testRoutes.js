1f5c0e828bf3627432257f74fef280a287d9fe4e
dixit <dixitambaliya303@gmail.com>
2025-12-08 15:23:17 +0530

    .

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -2,9 +2,9 @@ const express = require("express")
 const router = express.Router()
 const { authenticationToken } = require("../middleware/authMiddleware")
 
-const {getMe} = require("../controller/testController.js")
+const { getMe, testInvoice } = require("../controller/testController.js")
 
-router.get("/me", authenticationToken, getMe); 
-router.get("/test", getMe); // Protected route
+router.get("/me", authenticationToken, getMe);
+router.get("/test", testInvoice); // Protected route
 
 module.exports = router;
