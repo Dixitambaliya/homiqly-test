cc4c4bdb86b7d3bddc851db0277479bc23a4f586
dixit <dixitambaliya303@gmail.com>
2025-12-09 17:55:54 +0530

    .

--- config/utils/email/encryption.js
+++ config/utils/email/encryption.js
@@ -2,13 +2,12 @@ const crypto = require("crypto");
 
 const ALGO = "aes-256-cbc";
 const SECRET_KEY = Buffer.from(process.env.RESPONSE_ENCRYPTION_KEY, "hex");
-const IV = crypto.randomBytes(16);
 
 const encryptResponse = (data) => {
-    const jsonString = JSON.stringify(data);
+    const IV = crypto.randomBytes(16); 
 
-    const cipher = crypto.createCipheriv(ALGO, Buffer.from(SECRET_KEY), IV);
-    let encrypted = cipher.update(jsonString, "utf8", "base64");
+    const cipher = crypto.createCipheriv(ALGO, SECRET_KEY, IV);
+    let encrypted = cipher.update(JSON.stringify(data), "utf8", "base64");
     encrypted += cipher.final("base64");
 
     return {
