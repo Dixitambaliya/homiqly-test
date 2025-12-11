50500a26403276bebbb18d8d4d40fb0575159ca1
dixit <dixitambaliya303@gmail.com>
2025-12-11 17:02:40 +0530

    .

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -4,6 +4,7 @@ const { authenticationToken } = require("../middleware/authMiddleware");
 const { adminOnly } = require("../middleware/adminAuth");
 const { setAdminCode, getAdminCode, editAdminCode } = require("../controller/superAdminController");
 
+
 router.post("/set-code", authenticationToken, adminOnly, setAdminCode);
 router.get("/get-code", authenticationToken, adminOnly, getAdminCode);
 router.patch("/edit-code", authenticationToken, adminOnly, editAdminCode);
